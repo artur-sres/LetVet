4b140c3ad2c502db9d09f60bb36b9634dc250f8b
Artur <asr.engsoft@gmail.com>
2025-10-03 07:43:56 -0300

    Class Data

---
+++ VetLet.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import pet.auxiliares.Data;
+public class VetLet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==================================================");
+        System.out.println("=          Bem-vindo ao sistema VetLet!          =");
+        System.out.println("==================================================");
+        menuEscolha(scanner);
+        scanner.close();
+    }
+    
+    public static void menuEscolha(Scanner scanner) {
+        
+        System.out.println("Escolha uma opção:");
+        System.out.println("0 - Cadastrar novo pet");
+        System.out.println("1 - Mostrar pets cadastrados");
+        System.out.println("2 - Sair");
+        int escolha = scanner.nextInt();
+        
+        switch (escolha) {
+            case 0:
+                System.out.println("Para cadastrar um novo pet, por favor, forneça as informações necessárias:");
+                
+                break;
+            case 1:
+                System.out.println("Mostrando pets cadastrados...");
+                break;
+            case 2:
+                System.out.println("Saindo...");
+                break;  
+            default:
+                System.out.println("Opção inválida. Saindo...");
+                break;
+        }
+    }
+}
+
+//Data dataNascimento = Data.dataReader(scanner);
--- pet/auxiliares/Data.java
+++ pet/auxiliares/Data.java
@@ -1,6 +1,74 @@
 package pet.auxiliares;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Data {
     int dia;
     int mes;
     int ano;
+    String dataString;
+
+    //Construtor
+    public Data(int dia, int mes, int ano, String dataString) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.dataString = dataString;
+    }
+
+    //Getters
+    public int getDia() {return dia;}
+    public int getMes() {return mes;}
+    public int getAno() {return ano;}
+    public String getDataString() {return dataString;}
+
+    //Data reader with validation
+    public static Data dataReader(Scanner scanner){
+        int dia, mes, ano;
+        while (true) {
+            //Day validation
+            try{
+                System.out.print("Dia: ");
+                dia = scanner.nextInt();
+                if(dia < 1 || dia > 31) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido para o dia.");
+                    continue;
+                }
+            }catch(InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, insira um número válido para o dia.");
+                scanner.next(); 
+                continue;
+            }
+
+            //Month validation
+            try{
+                System.out.print("Mês: ");
+                mes = scanner.nextInt();
+                if(mes < 1 || mes > 12) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido para o mês.");
+                    continue;
+                }
+            }catch(InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido para o mês.");
+                    scanner.next(); 
+                    continue;
+            }
+
+            //Year validation
+            try{
+                System.out.print("Ano: ");
+                ano = scanner.nextInt();
+                if(ano < 1900 || ano > 2025) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido para o ano.");
+                    continue;
+                }
+            }catch(InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido para o ano.");
+                    scanner.next(); 
+                    continue;
+            }
+            String dataString = dia + "/" + mes + "/" + ano;
+            return new Data(dia, mes, ano, dataString);
+        }
+    }
 }
